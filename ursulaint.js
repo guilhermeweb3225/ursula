76750f09c8f3e41f3a0afa1ded3b9efe719828c1
Guilherme P.Oliveira <guilhermeponciano3225@gmail.com>
2024-09-16 16:20:13 -0300

    atu6

--- ursulaint.js
+++ ursulaint.js
@@ -1,6 +1,7 @@
 var img2h=document.getElementById('img2h')
 var bt_tema=document.getElementById('dv-tema')
 var bt_tema_circulo=document.getElementById('dv-tema-circulo')
+
 //anicializaçao do menu
 function menuanimadoabrir(){
     var menu=document.querySelector('div.dv-menu')
@@ -33,29 +34,23 @@ function menuanimadofechar(){
 //tema claro e escuro
 function claro(){
     bt_tema_circulo.addEventListener('click',()=>{
-        let sinalT=document.getElementById('sinal-l-d')
         bt_tema_circulo.classList.remove('dv-temaB')
         bt_tema_circulo.classList.add('dv-temaW')
         bt_tema_circulo.style.left='100%'
         bt_tema_circulo.style.transform='translate(-100%)'
         bt_tema_circulo.style.background='white'
         bt_tema.style.background='#ff0000'
-        sinalT.style.fontSize='60px'
-        sinalT.style.color='white'
         escuro()
     })
 }
 claro()
     function escuro(){
     bt_tema_circulo.addEventListener('click',()=>{
-        let sinalT=document.getElementById('sinal-l-d')
         bt_tema_circulo.classList.add('dv-temaB')
         bt_tema_circulo.style.left='0%'
         bt_tema_circulo.style.transform='translate(0%)'
         bt_tema_circulo.style.background='black'
         bt_tema.style.background='#700045'
-        sinalT.style.fontSize='60px'
-        sinalT.style.color='black'
         claro()
     })
 }
